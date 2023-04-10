dad7b1bd4e6e2646be98606b84ea47670f39d6dc
shibohu <hushibo456@gmail.com>
2023-04-10 19:28:30 -0400

    z

--- react-app/src/components/SideNavBar/index.js
+++ react-app/src/components/SideNavBar/index.js
@@ -9,8 +9,18 @@ function SideNavBar(){
     return (
         <div className='main-side-navbar'>
 
+        <div className="side-bar-div-boxes">
+
         <i class="fa-sharp fa-solid fa-house"></i>
+        <p>Home</p>
+        </div>
+
+        <div className="side-bar-div-boxes">
+
         <i class="fa-solid fa-user"></i>
+        <p>User</p>
+        </div>
+
         </div>
     )
 }
