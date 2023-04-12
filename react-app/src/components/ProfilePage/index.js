11fd790e1fcdc544931b7ed8ebcadd04f30458d1
shibohu <hushibo456@gmail.com>
2023-04-12 11:36:11 -0400

    z

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -31,6 +31,10 @@ function ProfilePage(){
         dispatch(currentUserComment())
     },[dispatch])
     
+
+    if(!currentUser){ 
+        return <h1>need login</h1>
+    }
     const replaceIconIfNull = () => { 
         if (!currentUser.icon){ 
             return <img className='user-icon-placeholder' src="https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png"></img>
