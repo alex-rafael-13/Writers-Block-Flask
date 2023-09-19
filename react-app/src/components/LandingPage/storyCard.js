6b937c0bc2c78e75505298b0e341a2bd5dfe01d5
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-19 01:38:01 -0700

    fixed default image

--- react-app/src/components/LandingPage/storyCard.js
+++ react-app/src/components/LandingPage/storyCard.js
@@ -4,21 +4,12 @@ import './landingPage.css'
 export default function StoryCard({story}){
 
 
-    // console.log(story)
-    let imgUrl
-    if(!story.image){
-        imgUrl = 'https://cdn.leadx.org/wp-content/uploads/2017/06/Storytelling.jpg' 
-    } else{
-        imgUrl = story.image
-    }
-
-    // console.log(story)
     return(
         <>
             <NavLink exact to={`/stories/${story.id}`}>
                 <div className="story-card">
                     <h3>{story.title}</h3>
-                    <img className='preview-image' src={imgUrl} alt='image.txt'></img>
+                    <img className='preview-image' src={story.image} alt='image.txt'></img>
                     <div className='author-name'>By {story.username}</div>
                     <div className='genres-cont'>
                         {story.genres.map(genre => (
