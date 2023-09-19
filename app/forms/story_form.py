967bb3a6b07c9343be9fd1048070c646e7d1d979
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-19 00:27:43 -0700

    aws upload online

--- app/forms/story_form.py
+++ app/forms/story_form.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, FileField, FieldList
 from wtforms.validators import DataRequired, Email, ValidationError
-
-
-
+from ..api.AWS_helpers import ALLOWED_EXTENSIONS
+from flask_wtf.file import FileAllowed
 
 
 class StoryForm(FlaskForm):
 
     title = StringField('title', validators=[DataRequired()])
     content = StringField('content', validators=[DataRequired()])
-    image = StringField('image')
+    image = FileField('image', validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
+    genres = StringField('genres',validators=[DataRequired()])
