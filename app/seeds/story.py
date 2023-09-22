d5e6b7772ae7a03bb598bfbdcdb1f64114631bef
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-09-21 23:58:18 -0700

    all default imgs in s3

--- app/seeds/story.py
+++ app/seeds/story.py
@@ -8,13 +8,13 @@ def seed_story():
         user_id = 1,
         title = 'The Echoes of Chronos',
         content = THE_ECHOES_OF_CHRONOS,
-        image = 'https://i.ibb.co/8DGT1mJ/DALL-E-2023-04-14-12-59-12.png'
+        image = 'https://writers-block-1.s3.us-west-1.amazonaws.com/default/echoes.png'
     )
     luke2 = Story(
         user_id = 1,
         title = 'The Whispering Shards',
         content = THE_WHISPERING_SHARDS,
-        image = 'https://i.ibb.co/m68KgMP/DALL-E-2023-04-14-13-16-39-Lyra-at-the-center-reaches-out-towards-the-swirling-vortex-of-the-Nexus-w.png'
+        image = 'https://writers-block-1.s3.us-west-1.amazonaws.com/default/whispering.png'
     )
     luke3 = Story(
         user_id = 1,
@@ -26,37 +26,37 @@ def seed_story():
         user_id = 2,
         title = 'Animatronics and Ghosts',
         content = ANIMATRONICS,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIwC6zQc51CaXL4kgiX0tc54yDxKqK7pXoZQ&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/freddys.jpeg'
     )
     matt2 = Story(
         user_id = 2,
         title = 'The End',
         content = THE_END,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQuDmgx3kWG47b3-HeML3Sn1-Yyzn9Zzqmww&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/minecraft.jpeg'
     )
     matt3 = Story(
         user_id = 2,
         title = 'The Rise of Matt Patt',
         content = MATT_PATT,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa37I8he8dce_RF-TbX37CDp9B2NjcsWqGXw&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/mattpat.jpeg'
     )
     jon1 = Story(
         user_id = 3,
         title = 'The King of The North',
         content = THE_KING_OF_THE_NORTH,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGF33IWwPuu2eNDbMAG70D_6y2282JgBpopQ&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/king-of-the-north.jpeg'
     )
     jon2 = Story(
         user_id = 3,
         title = 'The Tale of the Dragons',
         content = DRAGONS,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR28YUDKKngiDua3_T511Yxj3DGHYNcddNtA&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/dragons.jpeg'
     )
     jon3 = Story(
         user_id = 3,
         title = 'Dragon\'s Passion',
         content = DANI,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxkEPxvoxPVs2bPvUY3wZ0azT-PpRSRjVENg&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/daenerys.jpeg'
     )
     adam1 = Story(
         user_id = 4,
@@ -68,37 +68,37 @@ def seed_story():
         user_id = 4,
         title = 'A Night in Paris',
         content = NIGHT_IN_PARIS,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyqs5U7FztNvg-nuZTf0HZROtyuCNf_xF7SQ&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/paris.jpeg'
     )
     adam3 = Story(
         user_id = 4,
         title = 'My Life in New Orleans',
         content = LIFE_IN_NEW_ORLEANS,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgIPalxvQuGCGx3ZgX5a9Sudyto3uD0ZFrDA&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/nola.jpeg'
     )
     lewis1 = Story(
         user_id = 5,
         title = 'Path of a Champion',
         content = PATH_OF_CHAMPS,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUgrWvKUIOz0VCDWUPt54JpBErmV825-WeHg&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/champion.jpeg'
     )
     lewis2 = Story(
         user_id = 5,
         title = 'Breaking Barries',
         content = BREAKING_BARRIES,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlUe9fIlvOQ_WwSlfcl5tmBClp8MeyGy-o6Q&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/lewis.jpeg'
     )
     lewis3 = Story(
         user_id = 5,
         title = 'Brazil, My Love',
         content = BRAZIL,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpw2VkGetdddvE42AIWbOmHYOY3hCbtvItvg&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/brazil.jpeg'
     )
     lewis4 = Story(
         user_id = 5,
         title = 'Serena, The Ultimate Champion',
         content = SERENA,
-        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkKM_X_i4dlWdtdRyvmg6KhR0WHQu5013ohA&usqp=CAU'
+        image='https://writers-block-1.s3.us-west-1.amazonaws.com/default/serena.jpeg'
     )
     db.session.add(luke3)
     db.session.add(adam2)
